9ae63c17e8310b72616eb0f4e81c60a9aefeeaf3
ziya yıldırım <ziya.yildirim@aknon.comm>
2021-05-01 19:59:56 +0300

    config add

--- src/index.js
+++ src/index.js
@@ -4,4 +4,11 @@ const elvenShiledRecipe = {
 	refineMoonstone:4
 }
 
+const test = {
+	...elvenShiledRecipe,
+	leathger: 1,
+	refineMoonstone: 4
+}
+
 console.log(elvenShiledRecipe);
+console.log(test)
