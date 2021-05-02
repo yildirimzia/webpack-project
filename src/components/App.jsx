71a087d2ea0ef9d94b0fa866263c55d39e9ac89d
ziya yıldırım <ziya.yildirim@aknon.comm>
2021-05-02 18:11:23 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,7 +10,7 @@ function App() {
 			<section className="hero"></section>
 			<main>
 				<section>
-					<h1>SIKTIGMIN KOR MERTI VE FARE EGESI</h1>
+					<h1>hi react</h1>
 				</section>
 			</main>
 			<img src={sword} alt="sword" width="250" height=""/>
